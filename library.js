a96236cba6af4638988d9465b0c8dcc9f87cc290
Paul Ortiz <113127266+ProgrammerOPaul@users.noreply.github.com>
2023-05-02 14:57:05 -0400

    Fix typos

--- library.js
+++ library.js
@@ -1,14 +1,20 @@
-let myLibrary = [];
+/* eslint-disable no-unused-vars */
+const myLibrary = [];
 
-book (title, author, pages, read){
+function Book (title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
-    this.info = function(){
-        return(title + ' by ' + author + ', ' + pages + ', ' + read) 
-    }
-}
+    this.read = read;}
 
-const starWars = new book("Star Wars", "George Lucas", "2560 pages", "not read yet");
+// eslint-disable-next-line func-names
+Book.prototype.info = function(){
+    return(`${this.title  } by ${  this.author  }, ${  this.pages  }, ${  this.read}`) };
+
+
+
+
+const starWars = new Book("Star Wars", "George Lucas", "2560 pages", "not read yet");
+// eslint-disable-next-line no-console
 console.log(starWars.info());
+
